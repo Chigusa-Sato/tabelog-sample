e13b55b2740367022b703e50f8bf62396b988db8
佐藤 千草 <chi-sato@PC644.local>
2023-04-07 11:29:22 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Card msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import Card from './components/Card.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Card
+  }
+}
+</script>
